b17c65c3be4133ce247a723495767236b075e8ac
ronnielopez <ronnielopez503@gmail.com>
2021-03-08 23:42:02 -0600

    Update Login.vue

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -24,7 +24,7 @@
             <div class="block mt-4">
                 <label class="flex items-center">
                     <jet-checkbox name="remember" v-model="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Recuerda me</span>
+                    <span class="ml-2 text-sm text-gray-600">Recuerdame</span>
                 </label>
             </div>
 
